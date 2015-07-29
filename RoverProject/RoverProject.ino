1ff13d882d658624fdd3ddd5a352a2f6b3f5af48
MichaelGrupp <mg91@arcor.de>
2015-07-29 11:21:44 +0200

    created project with VS2013 and Visual Micro

---
+++ RoverProject/RoverProject.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		RoverProject.ino
+ Created:	29.07.2015 11:20:01
+ Author:	Michael
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
