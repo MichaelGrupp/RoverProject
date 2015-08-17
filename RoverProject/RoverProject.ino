c22bd7b559c2047f08eca2045bd35136a2cf9375
MichaelGrupp <mg91@arcor.de>
2015-08-17 18:54:30 +0200

    test program with US and IR sensors

--- RoverProject/RoverProject.ino
+++ RoverProject/RoverProject.ino
@@ -8,28 +8,54 @@
 
 Drive Chassis;
 Ultrasonic USSensor(2, 3);
+int IR_dist1 = 0;
+int IR_dist2 = 0;
+bool bumped = false;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
+  Serial.begin(9600);
 	USSensor.setUnit(Unit::CENTIMETERS);
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
-	USSensor.measure();
-	while (USSensor.inRange() && USSensor.getDistance() > 20 || USSensor.getDistance() == 0) {
-		Chassis.driveStraight(75, Direction::FORWARD);
-		delay(25);
-		USSensor.measure();
-	}
-	Chassis.stop();
-	delay(200);
-	Chassis.driveStraight(75, Direction::BACKWARD);
-	delay(3000);
-	Chassis.stop();
-	delay(200);
-	Chassis.turnLeft(100);
-	delay(2000);
-	Chassis.turnRight(100);
-	delay(2000);
+
+  IR_dist1 = analogRead(3);
+  IR_dist2 = analogRead(4);
+  USSensor.measure();
+  bumped = (USSensor.getDistance() > 5) && (USSensor.getDistance() < 20);
+  Serial.println(USSensor.getDistance());
+
+  if(bumped) {
+    Serial.println("bump 3");
+    Chassis.stop();
+    delay(50);
+    //Chassis.driveStraight(75, Direction::BACKWARD);
+    //delay(3000);
+    if (IR_dist1 > IR_dist2)
+      Chassis.turnLeft(150);
+    else
+      Chassis.turnRight(150);
+    delay(2000);
+  }
+  else if(IR_dist1 > 220) {
+    Serial.println("bump 1");
+    Chassis.stop();
+    delay(50);
+    Chassis.turnLeft(150);
+    delay(200);
+  }
+  else if(IR_dist2 > 220) {
+    Serial.println("bump 2");
+    Chassis.stop();
+    delay(50);
+	  Chassis.turnRight(150);
+    delay(200);
+  }
+  else {
+    Chassis.driveStraight(100, Direction::FORWARD);
+    delay(150);
+  }
+    
 }
